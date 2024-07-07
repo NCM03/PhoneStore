2571355a7ea3884b6c8163d5649131752717250f
NCM03 <cuongnmhe172187@fpt.edu.vn>
2024-07-07 13:45:23 +0700

    First Commit

---
+++ src/test/java/fa/training/phonestore/PhoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.training.phonestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
