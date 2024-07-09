de8c17e2a48881ec4cbe1f1d518739a3da58dca0
Hungphamlab <phamthuy091984@gmail.com>
2024-07-09 15:35:15 +0700

    Insert template

--- src/main/java/fa/training/phonestore/PhoneStoreApplication.java
+++ src/main/java/fa/training/phonestore/PhoneStoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PhoneStoreApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PhoneStoreApplication.class, args);
     }
 
