58bb2058bfde7f5ad5a4f8e1d4c9f5d660f71401
Hungphamlab <phamthuy091984@gmail.com>
2024-07-22 23:01:49 +0700

    .

--- src/main/java/fa/training/phonestore/entity/Account.java
+++ src/main/java/fa/training/phonestore/entity/Account.java
@@ -1,8 +1,6 @@
-package fa.training.phonestore.entity;
+package fa.training.phonestore.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import fa.training.phonestore.Constraint.EntityConstraint.DtoConstraint.Password;
-import fa.training.phonestore.Constraint.EntityConstraint.DtoConstraint.Username;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,30 +9,16 @@ import lombok.Data;
 @Table(name = "Account")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Account {
-    @Id
-    @Column(name = "AccountID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+@Id
+@Column(name = "AccountID")
     private int accountId;
-    @Column(name = "Username")
-    @Username
-    private String username;
-    @Column(name = "Password")
-    @Password
-    private String password;
-    @ManyToOne
-    @JoinColumn(name = "RoleID")
-    private Role role;
-    @Column(name = "IsActive")
-    private boolean isActive;
-
-    @PrePersist
-    public void prePersist() {
-        if (role == null) {
-            Role defaultRole = new Role();
-            defaultRole.setRoleId(3); // Thiết lập RoleID mặc định là 3
-            this.role = defaultRole;
-        }
-        setActive(true);
-    }
+@Column(name = "Username")
+private String username;
+@Column(name = "Password")
+private String password;
+@ManyToOne
+@JoinColumn(name = "RoleID")
+private fa.training.phonestore.Entity.Role role;
+@Column(name = "is_active")
+private boolean status;
 }
